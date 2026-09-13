93cae3276b953d49a21087412c4164275adf70dc
nikitha-16m <nikiths321@gamil.com>
2026-09-13 17:16:53 +0530

    to check even or odd

---
+++ evenorodd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a=9;
+    if(a%2==0){
+        printf("your number is even");
+    }
+    else{
+        printf("your number is odd");
+    }
+    return 0;
+}
