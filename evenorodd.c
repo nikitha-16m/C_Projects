f02ed6bb9652b69a0cc39759813572c06f9a73c7
nikitha-16m <nikiths321@gamil.com>
2026-09-13 17:21:23 +0530

    update evenorodd program with user input

--- evenorodd.c
+++ evenorodd.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main(){
-    int a=9;
+    int a;
+    printf("enter a number to find even or odd:");
+    scanf("%d",&a);
     if(a%2==0){
         printf("your number is even");
     }
