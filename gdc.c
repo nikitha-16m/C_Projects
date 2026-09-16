91cd5711f6f5a07d97b25e2a9aa0dce33278fb37
nikitha-16m <nikiths321@gamil.com>
2026-09-16 17:21:47 +0530

    perfectnum

---
+++ gdc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("enter two number:");
+    scanf("%d%d",&a,&b);
+    int i;
+    int largest=a;
+    int gdc=0;
+    if(b>a){
+        largest=b;
+    }
+    for(i=1;i<largest;i++){
+        if(a%i==0&&b%i==0){
+            gdc=i;
+        }
+        else{
+            continue;
+        }
+    }
+    printf("%d",gdc)
+}
