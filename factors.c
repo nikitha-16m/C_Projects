2d9293ca09d70a9c52a3d2da8faacd26fb413062
nikitha-16m <nikiths321@gamil.com>
2026-09-16 21:38:48 +0530

    factors of a number

--- factors.c
+++ factors.c
@@ -5,11 +5,14 @@ int main(){
     scanf("%d",&num);
     int i;
     int sum=0;
+    int sum1=0;
     for(i=1;i<=num;i++){
         if(num%i==0){
             sum+=1;
+            sum1+=i;
         }
     }
-    printf("the no of factors are :%d",sum);
+    printf("the no of factors are :%d\n",sum);
+    printf("the sum of the factors are :%d",sum1);
     return 0;
 }
